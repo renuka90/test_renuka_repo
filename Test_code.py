995a8884132d403ee54beb2c5f7cdaa800d10201
renuka90 <renukagurung90@gmail.com>
2020-06-09 13:42:24 +0200

    file added

---
+++ Test_code.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  9 13:32:02 2020
+
+@author: ICTO-EB
+"""
+
+
+"""
+Created on Mon Feb  3 01:59:14 2020
+
+@author: Renu
+"""
+import streamlit as st
+import pandas as pd
+from gensim.models import Word2Vec
+
+st.title('Search tool: Journal Of Management_120 pdfs')
+pos_str = st.text_input('Enter keyword(s)')
+
+
+
+model = Word2Vec.load('C:/Thesis/Data/save/Master_Data/Model/save_model1_with_6_articles.model')
+# TRAIN THE WORD2VEC MODEL ON THE DATA
+
+
+if st.button("Search"):
+    result = pd.DataFrame(model.wv.most_similar(positive = pos_str, topn=10), columns = ['word', 'similarity'])
+    st.text(result)
